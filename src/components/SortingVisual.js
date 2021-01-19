89752f1e326165e963601cc4adb99f18a294969d
Danny <dannyzamora1996@gmail.com>
2021-01-18 23:24:47 -0800

    stuck

--- src/components/SortingVisual.js
+++ src/components/SortingVisual.js
@@ -1,38 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import PropTypes from "prop-types";
 
 import * as sorting from "./algorithms/algorithms";
 
 const SortingVisual = (props) => {
   const [array, setArray] = useState([]);
+  const containerRef = useRef(null);
   //const [width, setWidth] = React.useState(window.innerWidth);
 
   useEffect(() => {
     initialArray();
 
-    //https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
-    // const handleWindowResize = () => setWidth(window.innerWidth);
-    // window.addEventListener("resize", handleWindowResize);
-    // return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
 
   const initialArray = () => {
+    clearInterval()
     const array = [];
-    for (let i = 0; i < window.innerWidth / 10; i++) {
-      array.push(randomInt(1, 55));
+    for (let i = 0; i < 10; i++) {
+      array.push(randomInt(1, 500));
     }
+
+
+
     setArray(array);
   };
 
   const mergeSort = () => {};
-  const bubbleSort = () => {
-    const jsSort = array.sort((a, b) => a - b);
-    const sortedArr = sorting.bubbleSort(array);
-    console.log(jsSort);
-    console.log(sortedArr);
 
-    console.log(arraysAreEqual(jsSort, sortedArr));
+  const bubbleSort = () => {
+    const animations = sorting.bubbleSort(array);
+    console.log(array)
+    console.log(animations)
+    animations.forEach(([comparison,swapped],i)=>{
+      setTimeout(()=>{
+        if(!swapped) {
+          if (comparison.length === 2) {
+            const [i, j] = comparison;
+            animateArrayAccess(i);
+            animateArrayAccess(j);
+          } else { 
+            const [i] = comparison;
+            console.log(i)
+            animateArrayAccess(i);
+          }
+        }
+        else{
+          setArray(prev=> {
+            const [k,newValue] = comparison;
+            const newArray = [...prev];
+            newArray[k]=newValue;
+            return newArray;
+          })
+        }
+      },i*1500)
+    })
+    // setTimeout(()=>{
+    //   animateSortedArray();
+    // },animations.length*500)
+
   };
+
+  // function animateSortedArray() {
+  //   const arrayBars = containerRef.current.children;
+  //   for (let i = 0; i < arrayBars.length; i++) {
+  //     const arrayBarStyle = arrayBars[i].style;
+  //     setTimeout(
+  //       () => (arrayBarStyle.backgroundColor = "purple"),
+  //       i * 500,
+  //     );
+  //   }
+  //   setTimeout(() => {
+      
+  //   }, arrayBars.length * 500);
+  // }
+
 
   const quickSort = () => {};
 
@@ -51,23 +92,39 @@ const SortingVisual = (props) => {
     }
   };
 
+
+  function animateArrayAccess(index) {
+    const arrayBars = containerRef.current.children;
+    const arrayBarStyle = arrayBars[index].style;
+    setTimeout(() => {
+      arrayBarStyle.backgroundColor = "Green";
+    }, 1500);
+    setTimeout(() => {
+      arrayBarStyle.backgroundColor = '';
+    }, 1500 * 2);
+  }
+
   return (
-    <div className="array">
-      {array.map((val, i) => (
+    <div>
+      <div className="array" ref={containerRef}>
+        {array.map((val, i) => (
         <div
           className="array__bar"
           key={i}
-          style={{ height: `${val}vh` }}
-        ></div>
-      ))}
+          style={{ height: `${val}px` }}
+          />
+        ))}
 
-      <button onClick={initialArray}>New Array</button>
-      <button onClick={mergeSort}>Merge Sort</button>
-      <button onClick={bubbleSort}>Bubble Sort</button>
-      <button onClick={quickSort}>Quick Sort</button>
-      <button onClick={heapSort}>Heap Array</button>
-      <button onClick={testAlg}>Test</button>
+      
     </div>
+    <button onClick={initialArray}>New Array</button>
+    <button onClick={mergeSort}>Merge Sort</button>
+    <button onClick={bubbleSort}>Bubble Sort</button>
+    <button onClick={quickSort}>Quick Sort</button>
+    <button onClick={heapSort}>Heap Array</button>
+    <button onClick={testAlg}>Test</button>
+    </div>
+  
   );
 };
 
--- src/components/algorithms/algorithms.js
+++ src/components/algorithms/algorithms.js
@@ -1,13 +1,28 @@
-export const bubbleSort = (inputArr) => {
-  let len = inputArr.length;
+export const bubbleSort = (array) => {
+  const copy = [...array]
+  const animations = [];
+  let len = copy.length; // [1,2] 
   for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len; j++) {
-      if (inputArr[j] > inputArr[j + 1]) {
-        let tmp = inputArr[j];
-        inputArr[j] = inputArr[j + 1];
-        inputArr[j + 1] = tmp;
+    for (let j = 0; j < len-i-1; j++) {
+
+      animations.push([[j,j+1],false]); 
+
+      if (copy[j] > copy[j + 1]) {
+        let tmp = copy[j];
+        copy[j] = copy[j + 1];      
+        animations.push([[j,copy[j]],true]); 
+        copy[j + 1] = tmp;
+        animations.push([[j+1,tmp],true]); 
       }
+
+    
+
+
     }
+
+
+
+
   }
-  return inputArr;
+  return animations;
 };
